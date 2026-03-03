1fd034e9575331eeba8cb0e87d4787f54bdf6255
shubhamxdhapola <shubhamdhapola143@gmail.com>
2026-03-04 01:14:32 +0530

    Updated advance contoller

--- backend/controllers/advance.controller.js
+++ backend/controllers/advance.controller.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import Advance from "../models/advance.model.js";
+import SalaryRecord from '../models/salary.record.model.js'
 
 export const getAdvances = async (req, res) => {
     try {
@@ -22,7 +24,7 @@ export const getEmployeeAdvances = async (req, res) => {
 
         const advances = await Advance.find({ employeeId, employerId })
         return res.status(200).json({ advances })
-        
+
     } catch (error) {
         console.log("Error in getEmployeeAdvances controller : ", error)
         return res.status(500).json({ message: "Internal server error" })
@@ -70,6 +72,24 @@ export const updateAdvance = async (req, res) => {
             return res.status(400).json({ message: "Invalid advance id" });
         }
 
+        const advance = await Advance.findOne({ _id: id, employerId })
+
+        if (!advance) {
+            return res.status(404).json({ message: "Advance not exists" })
+        }
+
+        const month = advance.date.toISOString().substring(0, 7)
+
+        const isSalaryGenerated = await SalaryRecord.findOne(
+            { employeeId: advance.employeeId, employerId, month }
+        )
+
+        if (isSalaryGenerated) {
+            return res.status(400).json({
+                message: "Cannot update advance. Salary already generated for this month"
+            })
+        }
+
         const updates = {}
         if (amount) updates.amount = amount
         if (note) updates.note = note
@@ -99,12 +119,27 @@ export const deleteAdvance = async (req, res) => {
         if (!mongoose.Types.ObjectId.isValid(id)) {
             return res.status(400).json({ message: "Invalid advance id" });
         }
+        const advance = await Advance.findOne({ _id: id, employerId })
 
-        const advance = await Advance.findOneAndDelete({ _id: id, employerId })
         if (!advance) {
-            return res.status(400).json({ message: "Advance not exists" })
+            return res.status(404).json({ message: "Advance not exists" })
         }
+
+        const month = advance.date.toISOString().substring(0, 7)
+
+        const isSalaryGenerated = await SalaryRecord.findOne(
+            { employeeId: advance.employeeId, employerId, month }
+        )
+
+        if (isSalaryGenerated) {
+            return res.status(400).json({
+                message: "Cannot delete advance. Salary already generated for this month"
+            })
+        }
+
+        await advance.deleteOne()
         return res.status(200).json({ message: "Advance deleted successfully" })
+
     } catch (error) {
         console.log("Error in deleteAdvance controller : ", error)
         return res.status(500).json({ messge: "Internal sever error" })
